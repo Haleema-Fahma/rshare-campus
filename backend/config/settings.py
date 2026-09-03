3b2bde1e44885c992633a3da1c11a10d2e3aec10
Haleema-Fahma <haleemafahmamc@gmail.com>
2026-09-03 23:16:31 +0530

    Create RShare Django application modules

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'accounts',
+    'listings',
+    'exchanges',
+    'credits',
+    'rewards',
+    'notifications',
 ]
 
 MIDDLEWARE = [
